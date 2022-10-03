d0f1d354a238134a35eff85d4198ff247f7fc50a
Vladislav Mindubaev <vladislavik36@yandex.ru>
2022-10-03 19:44:07 +0300

    fixed

--- passw_gen.cpp
+++ passw_gen.cpp
@@ -6,7 +6,7 @@ std::string PasswordGenerator::Generate(int idx)
 	for (int i = 0; i < max_c; i++)
 	{
 		out += symbols[idx % symbols.size()];
-		idx = idx / (symbols.size());
+		idx /= (symbols.size());
 	}
 	std::reverse(out.begin(), out.end());
 	return out;
