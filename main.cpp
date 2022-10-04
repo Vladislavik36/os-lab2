714eadc6fa6f837b87a84873c601b946d12c69c2
Vladislav Mindubaev <vladislavik36@yandex.ru>
2022-10-04 09:30:20 +0300

    bug fixes

--- main.cpp
+++ main.cpp
@@ -6,8 +6,6 @@
 #include "hashers.hpp"
 
 void CrackSha256() {
-	auto start_t = std::chrono::steady_clock::now();
-
 	std::vector<std::string> hashes = {
 		"1115dd800feaacefdf481f1f9070374a2a81e27880f187396db67958b207cbad",
 		"3a7bd3e2360a3d29eea436fcfb7e44c735d117c42d1c1835420b6b9942dd4f1b",
@@ -21,6 +19,11 @@ void CrackSha256() {
 	if (n_threads == 0)
 		throw std::runtime_error("You should enter threads more then 0.");
 
+	auto max_threads = std::thread::hardware_concurrency();
+	if (n_threads > std::thread::hardware_concurrency())
+		throw std::runtime_error("You should enter threads less or equal " + std::to_string(max_threads) + ".");
+
+	auto start_t = std::chrono::steady_clock::now();
 	auto cracker = HashCracker<Sha256Hasher>(5);
 	cracker.Crack(hashes, n_threads);
 
@@ -31,12 +34,10 @@ void CrackSha256() {
 }
 
 void CrackMD5() {
-	auto start_t = std::chrono::steady_clock::now();
-
 	std::vector<std::string> hashes = {
-		"147538da338b770b61e592afc92b1ee6",
-		"90e9b2d33384f266345517aed3b17382",
-		"bf54c15ee365c3e73a7f6af614611347"
+		"81d45c9cf678fbaa8d64a6f29a6f97e3",
+		"1f3870be274f6c49b3e31a0c6728957f",
+		"d9308f32f8c6cf370ca5aaaeafc0d49b"
 	};
 
 	std::cout << "Enter threads count for MD5: " << std::endl;
@@ -46,6 +47,11 @@ void CrackMD5() {
 	if (n_threads == 0)
 		throw std::runtime_error("You should enter threads more then 0.");
 
+	auto max_threads = std::thread::hardware_concurrency();
+	if (n_threads > std::thread::hardware_concurrency())
+		throw std::runtime_error("You should enter threads less or equal " + std::to_string(max_threads) + ".");
+
+	auto start_t = std::chrono::steady_clock::now();
 	auto cracker = HashCracker<MD5Hasher>(5);
 	cracker.Crack(hashes, n_threads);
 
